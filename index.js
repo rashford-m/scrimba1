01a778f3007ff8a8183aa060244f68c9c4d67afb
Rashford <mikearowo@gmail.com>
2023-05-11 20:45:36 +0100

    updated

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-document.getElementById("count-el").innerHTML = 5;
-commit 
+// document.getElementById("count-el").innerHTML = 5;
+
+let count = 0
+console.log(count)
+
+let myAge = 24
+console.log(myAge)
